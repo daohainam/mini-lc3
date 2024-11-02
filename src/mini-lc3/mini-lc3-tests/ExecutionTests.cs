bf8a09aa2acea495e602fe66ccd59652fe12e01a
Dao Hai Nam <daohainam@gmail.com>
2024-11-02 22:28:10 +0100

    Update Reverse_String_Test

--- src/mini-lc3/mini-lc3-tests/ExecutionTests.cs
+++ src/mini-lc3/mini-lc3-tests/ExecutionTests.cs
@@ -53,23 +53,59 @@ namespace mini_lc3_tests
         {
             var instructions = new ushort[] {
                 0xE012,0x123F,0x6641,0x0402,0x1261,0x0FFC,0x943F,0x1481,0x14A0,0x0808,0x6600,0x6840,0x7800,
-                0x7640,0x1021,0x127F,0x14BE,0x0FF6,0xF025,0x0054,0x0068,0x0069,0x0073,0x0020,0x0069,0x0073,
-                0x0020,0x0073,0x006F,0x0020,0x006D,0x0075,0x0063,0x0068,0x0020,0x0066,0x0075,0x006E,0x0021,0x0000
+                0x7640,0x1021,0x127F,0x14BE,0x0FF6,0xF025,
+                0x0041,0x0042,0x0043,0x0044,0x0045,0x0046,0x0047,0x0048,0x0049,0x004A,0x004B,0x004C,0x004D,
+                0x0000
             };
+
+            /*
+                .ORIG    x3000
+                rev LEA R0,FILE      ;; R0 is beginning of string
+                        ADD      R1,R0,#-1    
+                LOOP1 LDR R3,R1,#1     ;; Note -- LDR "looks" at the word past R1
+                        BRz      DONE1
+                        ADD      R1,R1,#1
+                        BR       LOOP1
+
+                DONE1 NOT R2,R0
+                        ADD      R2,R2,R1
+
+                LOOP2 ADD R2,R2,#0
+                        BRn      DONE2
+                        LDR      R3,R0,#0     ;; Swap
+                        LDR      R4,R1,#0
+                        STR      R4,R0,#0
+                        STR      R3,R1,#0
+                        ADD      R0,R0,#1     ;; move pointers
+                        ADD      R1,R1,#-1
+                        ADD      R2,R2,#-2    ;; decrease R2 by 2
+                        BR       LOOP2
+
+                DONE2 HALT
+
+                FILE    .STRINGZ "ABCDEFGHIJKLM"
+                        .END
+             */
 
             _machine.Memory.LoadInstructions(instructions);
             _machine.Memory.LoadInstructions([0x1F1F], 0x25); // HALT (TRAP x25)
             _machine.CPU.FetchAndExecuteUntilHalt();
 
-            _machine.CPU.ALU.RegisterFile.R0.Should().Be(0x3014);
-            _machine.CPU.ALU.RegisterFile.R1.Should().Be(0x3014);
+            _machine.CPU.ALU.RegisterFile.R0.Should().Be(0x3019);
+            _machine.CPU.ALU.RegisterFile.R1.Should().Be(0x3019);
             _machine.CPU.ALU.RegisterFile.R2.Should().Be(-0x0001);
-            _machine.CPU.ALU.RegisterFile.R3.Should().Be(0x54);
-            _machine.CPU.ALU.RegisterFile.R4.Should().Be(0x69);
+            _machine.CPU.ALU.RegisterFile.R3.Should().Be(0x46);
+            _machine.CPU.ALU.RegisterFile.R4.Should().Be(0x48);
             _machine.CPU.ControlUnit.PC.Should().Be(0x1F1F);
             _machine.CPU.ControlUnit.N.Should().BeTrue();
             _machine.CPU.ControlUnit.Z.Should().BeFalse();
             _machine.CPU.ControlUnit.P.Should().BeFalse();
+
+            var output = "ABCDEFGHIJKLM".Reverse().ToArray();
+            for (int i = 0; i < output.Length; i++)
+            {
+                _machine.Memory[0x3013 + i].Should().Be((short)output[i]);
+            }
         }        
         
         private void LoadAndExecuteInstructions(ushort[] instructions)
